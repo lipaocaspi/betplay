49d3fdfb7feff10716e9b761346c8a093d18cfe4
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-06-17 22:22:24 -0500

    Update classes

--- src/main/java/com/paola/betplay/medic/domain/entities/Medic.java
+++ src/main/java/com/paola/betplay/medic/domain/entities/Medic.java
@@ -5,85 +5,10 @@ public class Medic {
     private String name;
     private int age;
     private String nationality;
-    private int idTeam;
-    private String medicType;
     private String degree;
     private int experience;
+    private String medicType;
+    private int idTeam;
 
     public Medic() {}
-
-    public Medic(int id, String name, int age, String nationality, int idTeam, String medicType, String degree, int experience) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.nationality = nationality;
-        this.idTeam = idTeam;
-        this.medicType = medicType;
-        this.degree = degree;
-        this.experience = experience;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public int getIdTeam() {
-        return idTeam;
-    }
-
-    public void setIdTeam(int idTeam) {
-        this.idTeam = idTeam;
-    }
-
-    public String getMedicType() {
-        return medicType;
-    }
-
-    public void setMedicType(String medicType) {
-        this.medicType = medicType;
-    }
-
-    public String getDegree() {
-        return degree;
-    }
-
-    public void setDegree(String degree) {
-        this.degree = degree;
-    }
-
-    public int getExperience() {
-        return experience;
-    }
-
-    public void setExperience(int experience) {
-        this.experience = experience;
-    }
 }
--- src/main/java/com/paola/betplay/player/domain/entities/Player.java
+++ src/main/java/com/paola/betplay/player/domain/entities/Player.java
@@ -5,115 +5,13 @@ public class Player {
     private String name;
     private int age;
     private String nationality;
-    private int idTeam;
     private int shirtNumber;
     private String role;
     private String dateOfArrival;
     private int goalsScored;
     private int yellowCards;
     private int redCards;
+    private int idTeam;
 
     public Player() {}
-
-    public Player(int id, String name, int age, String nationality, int idTeam, int shirtNumber, String role, String dateOfArrival, int goalsScored, int yellowCards, int redCards) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.nationality = nationality;
-        this.idTeam = idTeam;
-        this.shirtNumber = shirtNumber;
-        this.role = role;
-        this.dateOfArrival = dateOfArrival;
-        this.goalsScored = goalsScored;
-        this.yellowCards = yellowCards;
-        this.redCards = redCards;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public int getIdTeam() {
-        return idTeam;
-    }
-
-    public void setIdTeam(int idTeam) {
-        this.idTeam = idTeam;
-    }
-
-    public int getShirtNumber() {
-        return shirtNumber;
-    }
-
-    public void setShirtNumber(int shirtNumber) {
-        this.shirtNumber = shirtNumber;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getDateOfArrival() {
-        return dateOfArrival;
-    }
-
-    public void setDateOfArrival(String dateOfArrival) {
-        this.dateOfArrival = dateOfArrival;
-    }
-
-    public int getGoalsScored() {
-        return goalsScored;
-    }
-
-    public void setGoalsScored(int goalsScored) {
-        this.goalsScored = goalsScored;
-    }
-
-    public int getYellowCards() {
-        return yellowCards;
-    }
-
-    public void setYellowCards(int yellowCards) {
-        this.yellowCards = yellowCards;
-    }
-
-    public int getRedCards() {
-        return redCards;
-    }
-
-    public void setRedCards(int redCards) {
-        this.redCards = redCards;
-    }
 }
